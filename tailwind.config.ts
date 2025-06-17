b812b4e72bb84f271b11af75c63fa176879fab95
jess vanam <32110513+jvanam@users.noreply.github.com>
2025-06-17 17:37:17 -0400

    Add colors to tailwind, add .nvmrc

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,7 @@
 import type { Config } from "tailwindcss";
-const {
-  default: flattenColorPalette,
-} = require("tailwindcss/lib/util/flattenColorPalette");
 
 import svgToDataUri from "mini-svg-data-uri";
+import { record } from "zod";
 
 const config: Config = {
   content: [
@@ -14,93 +12,40 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        charcoal: "#08090A",
-        lightblack: "#1C1C1C",
-        secondary: "#E6E6E6",
-        muted: "var(--neutral-200)",
-      },
-      boxShadow: {
-        derek: `0px 0px 0px 1px rgb(0 0 0 / 0.06),
-        0px 1px 1px -0.5px rgb(0 0 0 / 0.06),
-        0px 3px 3px -1.5px rgb(0 0 0 / 0.06), 
-        0px 6px 6px -3px rgb(0 0 0 / 0.06),
-        0px 12px 12px -6px rgb(0 0 0 / 0.06),
-        0px 24px 24px -12px rgb(0 0 0 / 0.06)`,
-        aceternity: `0px 2px 3px -1px rgba(0,0,0,0.1), 0px 1px 0px 0px rgba(25,28,33,0.02), 0px 0px 0px 1px rgba(25,28,33,0.08)`,
-      },
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-      animation: {
-        move: "move 5s linear infinite",
-        "spin-circle": "spin-circle 3s linear infinite",
-      },
-      keyframes: {
-        move: {
-          "0%": { transform: "translateX(-200px)" },
-          "100%": { transform: "translateX(200px)" },
-        },
-        "spin-circle": {
-          "0%": { transform: "rotate(0deg)" },
-          "100%": { transform: "rotate(360deg)" },
-        },
+        main_public: "var(--main-public)",
+        main_public_tint: "var(--main-public-tint)",
+        main_school: "var(--main-school)",
+        main_school_tint: "var(--main-school-tint)",
+        main_white: "var(--main-white)",
+        main_black: "var(--main-black)",
+        main_seasonal: "var(--main-seasonal)",
+        record_news: "var(--record-news)",
+        record_exhibition: "var(--record-exhibitions)",
+        record_events: "var(--record-events)",
+        record_facilities: "var(--record-facilities)",
+        programmes_foundation: "var(--programmes-foundation)",
+        programmes_intermediate: "var(--programmes-intermediate)",
+        programmes_diploma: "var(--programmes-diploma)",
+        programmes_profpractice: "var(--programmes-profpractice)",
+        programmes_postgrad: "var(--programmes-postgrad)",
+        programmes_shortcourses: "var(--programmes-shortcourses)",
+        roam_light: "var(--roam-light)",
+        roam_dark: "var(--roam-dark)",
+        hooke_base: "var(--hooke-base)",
+        hooke_middle: "var(--hooke-middle)",
+        hooke_top: "var(--hooke-top)",
+        dta_highlight: "var(--dta-highlight)",
+        dta_people: "var(--dta-people)",
+        dta_intitutions: "var(--dta-intitutions)",
+        dta_practices: "var(--dta-practices)",
+        dta_collections: "var(--dta-collections)",
       },
     },
   },
   plugins: [
     require("tailwindcss-animate"),
     require("@tailwindcss/typography"),
-    addVariablesForColors,
-    function ({ matchUtilities, theme }: any) {
-      matchUtilities(
-        {
-          "bg-grid": (value: any) => ({
-            backgroundImage: `url("${svgToDataUri(
-              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
-            )}")`,
-          }),
-          "bg-grid-small": (value: any) => ({
-            backgroundImage: `url("${svgToDataUri(
-              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="8" height="8" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
-            )}")`,
-          }),
-          "bg-dot": (value: any) => ({
-            backgroundImage: `url("${svgToDataUri(
-              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="1.6257413380501518"></circle></svg>`
-            )}")`,
-          }),
-          "bg-dot-thick": (value: any) => ({
-            backgroundImage: `url("${svgToDataUri(
-              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="2.5"></circle></svg>`
-            )}")`,
-          }),
-        },
-        { values: flattenColorPalette(theme("backgroundColor")), type: "color" }
-      );
-
-      matchUtilities(
-        {
-          highlight: (value: any) => ({
-            boxShadow: `inset 0 1px 0 0 ${value}`,
-          }),
-        },
-        { values: flattenColorPalette(theme("backgroundColor")), type: "color" }
-      );
-    },
   ],
 };
-
-function addVariablesForColors({ addBase, theme }: any) {
-  let allColors = flattenColorPalette(theme("colors"));
-  let newVars = Object.fromEntries(
-    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
-  );
-
-  addBase({
-    ":root": newVars,
-  });
-}
 
 export default config;
