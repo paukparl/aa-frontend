f035e67a7cf1d7e682ca753b0047f61a8016236a
jess vanam <32110513+jvanam@users.noreply.github.com>
2025-06-17 23:54:08 -0400

    Add DTA font files; Optimize font loading using next/font; Rename DTA colors; Test fonts on home page; Add anti-aliasing to globals.css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,42 @@
 import "./globals.css";
+import localFont from 'next/font/local';
+
+// Optimize font loading using next/font to minimize layout shifts and prevent FOUC
+// Move to pages/_app.tsx if we decide on using pages router instead
+const diatype = localFont({
+  src: [
+    { path: './fonts/ABCDiatypeRoundedMono-Regular-Trial.woff2', weight: '400', style: 'normal' },
+    { path: './fonts/ABCDiatypeRoundedMono-RegularItalic-Trial.woff2', weight: '400', style: 'italic' },
+    { path: './fonts/ABCDiatypeRoundedMono-Bold-Trial.woff2', weight: '700', style: 'normal' },
+    { path: './fonts/ABCDiatypeRoundedMono-BoldItalic-Trial.woff2', weight: '700', style: 'italic' },
+  ],
+  variable: '--font-diatype',
+  display: 'swap',
+});
+
+const nhgdisplay = localFont({
+  src: [
+    { path: './fonts/NHaasGroteskDSPro-55Rg.ttf', weight: '400', style: 'normal' },
+    { path: './fonts/NHaasGroteskDSPro-56It.ttf', weight: '400', style: 'italic' },
+    { path: './fonts/NHaasGroteskDSPro-65Md.ttf', weight: '500', style: 'normal' },
+    { path: './fonts/NHaasGroteskDSPro-66MdIt.ttf', weight: '500', style: 'italic' },
+  ],
+  variable: '--font-nhgdisplay',
+  display: 'swap',
+});
+
+const nhgtext = localFont({
+  src: [
+    { path: './fonts/NHaasGroteskTXPro-55Rg.ttf', weight: '400', style: 'normal' },
+    { path: './fonts/NHaasGroteskTXPro-56It.ttf', weight: '400', style: 'italic' },
+    { path: './fonts/NHaasGroteskTXPro-65Md.ttf', weight: '500', style: 'normal' },
+    { path: './fonts/NHaasGroteskTXPro-66MdIt.ttf', weight: '500', style: 'italic' },
+    { path: './fonts/NHaasGroteskTXPro-75Bd.ttf', weight: '700', style: 'normal' },
+    { path: './fonts/NHaasGroteskTXPro-76BdIt.ttf', weight: '700', style: 'italic' },
+  ],
+  variable: '--font-nhgtext',
+  display: 'swap',
+});
 
 export default function RootLayout({
   children
@@ -6,7 +44,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html>
+    <html className={`${diatype.variable} ${nhgdisplay.variable} ${nhgtext.variable}`}>
       <body>
         {children}
       </body>
