ca69d0ea5bc6ee5934219a022d8e3e5b15a17847
Park <paukparliament@gmail.com>
2025-06-24 17:43:20 +0900

    chore: extend eslint-config-prettier and add escape pattern for no-unsued-vars

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,11 +6,29 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+  baseDirectory: __dirname
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript", "plugin:storybook/recommended"),
+  ...compat.config({
+    extends: [
+      "next/core-web-vitals",
+      "next/typescript",
+      "prettier",
+      "plugin:storybook/recommended"
+    ],
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+          destructuredArrayIgnorePattern: '^_',
+          varsIgnorePattern: '^_'
+        },
+      ]
+    }
+  })
 ];
 
 export default eslintConfig;
