ae8c57d397ff94761fdf0bfd9eefce6e78f7ae71
Park <paukparliament@gmail.com>
2025-06-25 15:29:04 +0900

    fix: font paths

--- fonts/index.ts
+++ fonts/index.ts
@@ -4,10 +4,10 @@ import localFont from 'next/font/local';
 // Move to pages/_app.tsx if we decide on using pages router instead
 const diatype = localFont({
   src: [
-    { path: './fonts/ABCDiatypeRoundedMono-Regular-Trial.woff2', weight: '400', style: 'normal' },
-    { path: './fonts/ABCDiatypeRoundedMono-RegularItalic-Trial.woff2', weight: '400', style: 'italic' },
-    { path: './fonts/ABCDiatypeRoundedMono-Bold-Trial.woff2', weight: '700', style: 'normal' },
-    { path: './fonts/ABCDiatypeRoundedMono-BoldItalic-Trial.woff2', weight: '700', style: 'italic' },
+    { path: './ABCDiatypeRoundedMono-Regular-Trial.woff2', weight: '400', style: 'normal' },
+    { path: './ABCDiatypeRoundedMono-RegularItalic-Trial.woff2', weight: '400', style: 'italic' },
+    { path: './ABCDiatypeRoundedMono-Bold-Trial.woff2', weight: '700', style: 'normal' },
+    { path: './ABCDiatypeRoundedMono-BoldItalic-Trial.woff2', weight: '700', style: 'italic' },
   ],
   variable: '--font-diatype-src',
   display: 'swap',
@@ -15,10 +15,10 @@ const diatype = localFont({
 
 const nhgdisplay = localFont({
   src: [
-    { path: './fonts/NHaasGroteskDSPro-55Rg.ttf', weight: '400', style: 'normal' },
-    { path: './fonts/NHaasGroteskDSPro-56It.ttf', weight: '400', style: 'italic' },
-    { path: './fonts/NHaasGroteskDSPro-65Md.ttf', weight: '500', style: 'normal' },
-    { path: './fonts/NHaasGroteskDSPro-66MdIt.ttf', weight: '500', style: 'italic' },
+    { path: './NHaasGroteskDSPro-55Rg.ttf', weight: '400', style: 'normal' },
+    { path: './NHaasGroteskDSPro-56It.ttf', weight: '400', style: 'italic' },
+    { path: './NHaasGroteskDSPro-65Md.ttf', weight: '500', style: 'normal' },
+    { path: './NHaasGroteskDSPro-66MdIt.ttf', weight: '500', style: 'italic' },
   ],
   variable: '--font-nhgdisplay-src',
   display: 'swap',
@@ -26,12 +26,12 @@ const nhgdisplay = localFont({
 
 const nhgtext = localFont({
   src: [
-    { path: './fonts/NHaasGroteskTXPro-55Rg.ttf', weight: '400', style: 'normal' },
-    { path: './fonts/NHaasGroteskTXPro-56It.ttf', weight: '400', style: 'italic' },
-    { path: './fonts/NHaasGroteskTXPro-65Md.ttf', weight: '500', style: 'normal' },
-    { path: './fonts/NHaasGroteskTXPro-66MdIt.ttf', weight: '500', style: 'italic' },
-    { path: './fonts/NHaasGroteskTXPro-75Bd.ttf', weight: '700', style: 'normal' },
-    { path: './fonts/NHaasGroteskTXPro-76BdIt.ttf', weight: '700', style: 'italic' },
+    { path: './NHaasGroteskTXPro-55Rg.ttf', weight: '400', style: 'normal' },
+    { path: './NHaasGroteskTXPro-56It.ttf', weight: '400', style: 'italic' },
+    { path: './NHaasGroteskTXPro-65Md.ttf', weight: '500', style: 'normal' },
+    { path: './NHaasGroteskTXPro-66MdIt.ttf', weight: '500', style: 'italic' },
+    { path: './NHaasGroteskTXPro-75Bd.ttf', weight: '700', style: 'normal' },
+    { path: './NHaasGroteskTXPro-76BdIt.ttf', weight: '700', style: 'italic' },
   ],
   variable: '--font-nhgtext-src',
   display: 'swap',
