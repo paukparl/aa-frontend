9d1747ab2dbfbab09b3c1ca7216bd705669defb1
jess vanam <32110513+jvanam@users.noreply.github.com>
2025-07-01 23:57:41 -0400

    center stories

--- stories/H1.stories.ts
+++ stories/H1.stories.ts
@@ -11,6 +11,7 @@ const meta = {
         component: " ",
       },
     },
+    layout: "centered",
   },
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ["autodocs"],
