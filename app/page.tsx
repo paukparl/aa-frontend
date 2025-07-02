0f103b155de63acd8354d5ca293e858273fdc8c3
jess vanam <32110513+jvanam@users.noreply.github.com>
2025-07-01 20:12:24 -0400

    remove event fetching

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,27 @@
 import { getEvent } from "@/api/getEvent";
 import { getEvents } from "@/api/getEvents";
+import { H2 } from "../components/Typography/H2";
 
 // import { notFound } from "next/navigation";
 
 export default async function HomePage() {
-  const events = await getEvents();
-  const event = await getEvent("0ti9pv2j81biuysjih7v7ilx1");
+  // const events = await getEvents();
+  // const event = await getEvent("0ti9pv2j81biuysjih7v7ilx1");
 
   // if (!event) notFound()
 
   return (
     <div>
-      <h1 className="font-nhg-display font-medium">Welcome to the Home Page</h1>
+      <h1 className="h1">Welcome to the Home Page</h1>
+      <H2 children="heree" />
       <p className="font-nhg-text italic">
         This is the main entry point of the application.
       </p>
       <pre className="font-diatype overflow-scroll">
-        {JSON.stringify(events, null, 2)}
+        {/* {JSON.stringify(events, null, 2)} */}
       </pre>
       <pre className="font-diatype overflow-scroll">
-        {JSON.stringify(event, null, 2)}
+        {/* {JSON.stringify(event, null, 2)} */}
       </pre>
     </div>
   );
