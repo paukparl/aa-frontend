93206740e28df0e609e7c2b4ad927437fdc83860
jess vanam <32110513+jvanam@users.noreply.github.com>
2025-07-01 23:59:00 -0400

    Add DTACarouselSlide component

---
+++ components/DTA/DTACarouselSlide.tsx
@@ -0,0 +1,30 @@
+// TODO
+// add width cutoff for caption
+import clsx from "clsx";
+import React from "react";
+import { Caption } from "../Typography/Caption";
+
+type DTACarouselSlideProps = {
+  imgSrc: string;
+  imgAlt: string;
+  className?: string;
+  caption?: string;
+};
+
+export const DTACarouselSlide = ({
+  imgSrc,
+  imgAlt,
+  caption,
+  className,
+}: DTACarouselSlideProps) => {
+  return (
+    <div className={clsx(className)}>
+      <img
+        src={imgSrc}
+        alt={imgAlt}
+        className="mb-1 h-[180px] w-auto sm:h-[370px] xl:h-[515px]"
+      />
+      {caption != null && <Caption children={caption} />}
+    </div>
+  );
+};
