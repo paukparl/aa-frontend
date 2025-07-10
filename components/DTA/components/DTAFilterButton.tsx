18258f8ed0c996489897979c92f69618b0bc6f5a
jess vanam <32110513+jvanam@users.noreply.github.com>
2025-07-10 14:55:55 -0400

    add dta filter button

---
+++ components/DTA/components/DTAFilterButton.tsx
@@ -0,0 +1,114 @@
+// TODO
+// add width cutoff for caption
+import clsx from "clsx";
+import React from "react";
+import { Mono } from "@/components/Typography/Mono";
+
+type filterState = "minimized" | "expanded" | "applied";
+type colorTheme = "people" | "practices" | "institutions" | "collections";
+type DTAFilterButtonProps = {
+  children: string;
+  filterState: filterState;
+  colorTheme: colorTheme;
+  className?: string;
+};
+
+export default ({
+  children,
+  className,
+  filterState,
+  colorTheme,
+}: DTAFilterButtonProps) => {
+  const colorThemeDict = {
+    people: {
+      stroke: "stroke-dta-people-foreground",
+      bg: "bg-dta-people-foreground",
+    },
+    practices: {
+      stroke: "stroke-dta-practices-foreground",
+      bg: "bg-dta-practices-foreground",
+    },
+    institutions: {
+      stroke: "stroke-dta-institutions-foreground",
+      bg: "bg-dta-institutions-foreground",
+    },
+    collections: {
+      stroke: "stroke-dta-collections-foreground",
+      bg: "bg-dta-collections-foreground",
+    },
+  };
+  return (
+    <div
+      className={clsx(
+        className,
+        (filterState === "expanded" || filterState === "applied") &&
+          `${colorThemeDict[colorTheme].bg} text-white`,
+        "flex grid h-[55px] w-[100%] grid-cols-[40px_1fr] items-center border border-dashed px-[25px]",
+      )}
+    >
+      {filterState === "minimized" && (
+        <svg
+          width="18"
+          height="18"
+          viewBox="0 0 21 21"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            className={colorThemeDict[colorTheme].stroke}
+            d="M0.0263672 10.5263L20.0264 10.5263"
+            stroke-width="5"
+            stroke-linejoin="round"
+          />
+          <path
+            className={colorThemeDict[colorTheme].stroke}
+            d="M10 0.526306L10 20.5263"
+            stroke-width="5"
+            stroke-linejoin="round"
+          />
+        </svg>
+      )}
+      {filterState === "expanded" && (
+        <svg
+          width="17"
+          height="5"
+          viewBox="0 0 20 5"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            className="stroke-white"
+            d="M0 2.5H20"
+            stroke-width="5"
+            stroke-linejoin="round"
+          />
+        </svg>
+      )}
+      {filterState === "applied" && (
+        <svg
+          width="18"
+          height="18"
+          viewBox="0 0 23 23"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            className="stroke-white"
+            d="M4.05273 3.55276L18.9999 18.5"
+            stroke-width="5"
+            stroke-linecap="square"
+            stroke-linejoin="round"
+          />
+          <path
+            className="stroke-white"
+            d="M19 3.55276L4.05279 18.5"
+            stroke-width="5"
+            stroke-linecap="square"
+            stroke-linejoin="round"
+          />
+        </svg>
+      )}
+      <Mono children={children} className="leading-0" />
+    </div>
+  );
+};
