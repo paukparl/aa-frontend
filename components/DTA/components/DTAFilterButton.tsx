96c9b104ada750e5aca3d6862bdc09ecf4c0433b
jess vanam <32110513+jvanam@users.noreply.github.com>
2025-07-13 23:24:53 -0400

    add alphabet filter component

--- components/DTA/components/DTAFilterButton.tsx
+++ components/DTA/components/DTAFilterButton.tsx
@@ -43,7 +43,7 @@ export default ({
         className,
         (filterState === "expanded" || filterState === "applied") &&
           `${colorThemeDict[colorTheme].bg} text-white`,
-        "flex grid h-[50px] w-[100%] cursor-pointer grid-cols-[35px_1fr] items-center border border-dashed px-[20px] sm:h-[55px] sm:grid-cols-[40px_1fr] sm:px-[25px]",
+        "grid h-[50px] w-[100%] cursor-pointer grid-cols-[35px_1fr] items-center border border-dashed px-[20px] sm:h-[55px] sm:grid-cols-[40px_1fr] sm:px-[25px]",
       )}
     >
       {filterState === "minimized" && (
@@ -105,7 +105,10 @@ export default ({
           />
         </svg>
       )}
-      <Mono children={children} className="leading-0" />
+      <Mono
+        children={children}
+        className="flex w-full justify-start leading-0"
+      />
     </div>
   );
 };
--- stories/DTAFilterAlphabet.stories.ts
+++ stories/DTAFilterAlphabet.stories.ts
@@ -1,28 +1,26 @@
 import type { Meta, StoryObj } from "@storybook/nextjs-vite";
-import { Mono } from "../components/Typography/Mono";
+import DTAFilterAlphabet from "@/components/DTA/components/DTAFilterAlphabet";
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
-  title: "Typography/Mono",
-  component: Mono,
+  title: "DTA/02 Layouts/Filters/Alphabet Filters",
+  component: DTAFilterAlphabet,
   parameters: {
     docs: {
       description: {
         component: " ",
       },
     },
-    layout: "centered",
   },
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
-  tags: ["autodocs"],
-  // More on argTypes: https://storybook.js.org/docs/api/argtypes
   argTypes: {
-    children: { control: "text" },
+    colorTheme: {
+      table: { disable: true },
+    },
     className: {
       table: { disable: true },
     },
   },
-} satisfies Meta<typeof Mono>;
+} satisfies Meta<typeof DTAFilterAlphabet>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
@@ -30,7 +28,8 @@ type Story = StoryObj<typeof meta>;
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Default: Story = {
   args: {
-    children: "Collection: Otto Koenigsberger Collection",
+    colorTheme: "people",
+    className: "px-[40px] pt-[30vh]",
   },
   globals: {
     backgrounds: { value: "light" },
