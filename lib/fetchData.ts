b3caf13ea41e6c39ef29f923a0cc902c1b8b1d70
Park <paukparliament@gmail.com>
2025-07-26 03:07:23 +0900

    feat: cache no-store for now

--- lib/fetchData.ts
+++ lib/fetchData.ts
@@ -112,6 +112,7 @@ async function fetchData(path: string, options: FetchDataOptions = {}) {
       "Content-Type": "application/json",
       ...headers,
     },
+    cache: "no-store", // TODO: replace this with cache strategy
     next,
   });
 }
