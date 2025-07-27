2492bd7f059cea1d9d20b506a7f3f21e17e360ee
jess vanam <32110513+jvanam@users.noreply.github.com>
2025-07-27 15:13:28 -0400

    complete multi select component

--- components/DTA/components/Filter/DTAFilterMultiSelectRow.tsx
+++ components/DTA/components/Filter/DTAFilterMultiSelectRow.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import clsx from "clsx";
-import React, { useState } from "react";
+import React, {  } from "react";
 import { DTAFilterMultiSelect } from "@/components/dta/components/Filter/DTAFilterMultiSelect";
 import { DTAFilterMultiSelectDropdown } from "@/components/dta/components/Filter/DTAFilterMultiSelectDropdown";
 
@@ -33,7 +33,7 @@ export const DTAFilterMultiSelectRow = ({
   toggleFilter,
   clearFilters,
 }: DTAFilterMultiRowProps) => {
-  const [activeIndex, setActiveIndex] = useState<number | null>(null);
+  const [activeIndex, setActiveIndex] = <number | null>(null);
   return (
     <div className="w-full">
       {/* Filter buttons in a row */}
--- components/School/Programmes/pages/PageDiplomaProgramme.tsx
+++ components/School/Programmes/pages/PageDiplomaProgramme.tsx
@@ -1,11 +1,13 @@
 import clsx from "clsx";
 import * as React from "react";
 import { ProgrammeTableInfo } from "@/components/School/Programmes/components/ProgrammeTableInfo";
+import { ProgrammeTableTerm } from "@/components/School/Programmes/components/ProgrammeTableTerm";
 import { ProgrammeTableUnits } from "@/components/School/Programmes/components/ProgrammeTableUnits";
 import { ProgrammeTextCol } from "@/components/School/Programmes/components/ProgrammeTextCol";
 import { Body } from "@/components/Typography/Body";
 import { H1 } from "@/components/Typography/H1";
 import { ButtonWindow } from "@/components/globals/components/ButtonWindow";
+import { Collapsible } from "@/components/globals/components/Collapsible";
 import { MediaGallery } from "@/components/globals/components/MediaGallery";
 import { TipinHeader } from "@/components/globals/layouts/TipinHeader";
 
@@ -110,6 +112,190 @@ export const PageDiplomaProgramme = ({
       tutors: "Nick Almond, Torange Khonsari, Mehrdad Seyf",
     },
   ];
+  const mockTermTableCreditsData = [
+    {
+      title: "Diploma Design Unit",
+      termYears: [1, 2],
+      core: false,
+      choose: "1 of 21 units",
+      credits: 70,
+    },
+    {
+      title: "History and Theory Studies",
+      termYears: [2],
+      core: false,
+      choose: "1 of 21 units",
+      credits: 60,
+    },
+    {
+      title: "Diploma Design Unit",
+      termYears: [1],
+      core: false,
+      choose: "1 of 9 courses",
+      credits: 30,
+    },
+    {
+      title: "Diploma Design Unit",
+      termYears: [1, 2],
+      core: true,
+      choose: "1 of 13 units",
+      credits: 10,
+    },
+    {
+      title: "Diploma Design Unit",
+      termYears: [1, 2],
+      core: true,
+
+      credits: 10,
+    },
+    {
+      title: "Diploma Design Unit",
+      termYears: [2],
+      core: true,
+      choose: "1 of 3 courses",
+      credits: 40,
+    },
+  ];
+  const mockTermTableLecturersData = [
+    {
+      title: "Diploma Design Unit",
+      lecturers: "Doreen Bernath",
+      termYears: [1],
+    },
+    {
+      title: "The Oceanic Feeling",
+      lecturers: "Doreen Bernath",
+      termYears: [1, 2],
+    },
+    {
+      title: "Diploma Design Unit",
+      lecturers: "Doreen Bernath",
+      termYears: [2],
+    },
+    {
+      title: "The Oceanic Feeling",
+      lecturers: "Doreen Bernath",
+      termYears: [1],
+    },
+  ];
+  const mockCollapsibleItems = [
+    {
+      header: "History and Theory Studies",
+      content: (
+        <div>
+          <ProgrammeTextCol>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum
+            dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+            ea commodo consequat. Duis aute irure dolor in reprehenderit in
+            voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+            officia deserunt mollit anim id est laborum.
+          </ProgrammeTextCol>
+          <ProgrammeTableTerm
+            className="mt-[20px]"
+            colorTheme="diploma"
+            type="lecturers"
+            units={mockTermTableLecturersData}
+          />
+        </div>
+      ),
+    },
+    {
+      header: "Environmental and Technical Studies",
+      content: (
+        <div>
+          <ProgrammeTextCol>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum
+            dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+            ea commodo consequat. Duis aute irure dolor in reprehenderit in
+            voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+            officia deserunt mollit anim id est laborum.
+          </ProgrammeTextCol>
+          <ProgrammeTableTerm
+            className="mt-[20px]"
+            colorTheme="diploma"
+            type="lecturers"
+            units={mockTermTableLecturersData}
+          />
+        </div>
+      ),
+    },
+    {
+      header: "Architectural Professional Practice",
+      content: (
+        <div>
+          <ProgrammeTextCol>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum
+            dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+            ea commodo consequat. Duis aute irure dolor in reprehenderit in
+            voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+            officia deserunt mollit anim id est laborum.
+          </ProgrammeTextCol>
+          <ProgrammeTableTerm
+            className="mt-[20px]"
+            colorTheme="diploma"
+            type="lecturers"
+            units={mockTermTableLecturersData}
+          />
+        </div>
+      ),
+    },
+    {
+      header: "Diploma Electives",
+      content: (
+        <div>
+          <ProgrammeTextCol>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum
+            dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+            ea commodo consequat. Duis aute irure dolor in reprehenderit in
+            voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+            officia deserunt mollit anim id est laborum.
+          </ProgrammeTextCol>
+          <ProgrammeTableTerm
+            className="mt-[20px]"
+            colorTheme="diploma"
+            type="lecturers"
+            units={mockTermTableLecturersData}
+          />
+        </div>
+      ),
+    },
+  ];
   return (
     <div
       className={clsx(
@@ -208,13 +394,22 @@ export const PageDiplomaProgramme = ({
             Architecture (ARB/RIBA Part 2) and the AA Diploma.
           </Body>
         </ProgrammeTextCol>
-        <span>TERM BY TERM TABLE</span>
+        <ProgrammeTableTerm
+          colorTheme="diploma"
+          type="credits"
+          units={mockTermTableCreditsData}
+        />
         <MediaGallery
           slides={mockCarouselSlides}
           colorTheme="school-programmes"
         />
         <ProgrammeTableUnits units={mockUnits} colorTheme="diploma" />
-        <span>STUDIES DROPDOWN</span>
+        <div>
+          <H1 tipIn className="mb-[20px] block">
+            Core Studies
+          </H1>
+          <Collapsible items={mockCollapsibleItems} />
+        </div>
       </div>
     </div>
   );
