08e48426cba570033946a72bbbaa9991d6a5bb21
Park <paukparliament@gmail.com>
2025-07-28 22:44:36 +0900

    fix: fix build error

--- components/dta/pages/DTAPersonPageContent.tsx
+++ components/dta/pages/DTAPersonPageContent.tsx
@@ -10,11 +10,11 @@ import {
   DTAObjectsGrid,
   DTAObjectsGridItem,
 } from "@/components/dta/components/DTAObjectsGrid";
+import { DTAPeopleTable } from "@/components/dta/components/DTAPeopleTable";
 import {
   DTAPracticesGrid,
   DTAPracticesGridItem,
 } from "@/components/dta/components/DTAPracticesGrid";
-import { DTATablePeople } from "@/components/dta/components/DTATablePeople";
 import { Map } from "@/components/dta/components/Map";
 import { cn } from "@/lib/cn";
 import { Schema } from "@/lib/schemas";
@@ -50,7 +50,7 @@ export const DTAPersonPageContent = ({ person }: DTAPersonPageContentProps) => {
       <div className="grid grid-cols-1 gap-[30px] md:grid-cols-2">
         <div>
           <DTAHeader className="capitalize">Education</DTAHeader>
-          <DTATablePeople
+          <DTAPeopleTable
             content={person.Eduction.map((row) => ({
               info: `${row.courseName ? `${row.courseName}, ` : ""}${row.institution ? `${row.institution}, ` : ""}${row.country ?? ""}`,
               year: row.dateRange ?? "",
@@ -59,7 +59,7 @@ export const DTAPersonPageContent = ({ person }: DTAPersonPageContentProps) => {
         </div>
         <div>
           <DTAHeader className="capitalize">Career</DTAHeader>
-          <DTATablePeople
+          <DTAPeopleTable
             content={person.Career.map((row) => ({
               info: `${row.jobRole ? `${row.jobRole}, ` : ""}${row.institution ? `${row.institution}, ` : ""}${row.country ?? ""}`,
               year: row.dateRange ?? "",
