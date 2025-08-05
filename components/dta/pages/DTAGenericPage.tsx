6fcb736b4388ade2eea95422f77e804b79f6dbb4
Park <paukparliament@gmail.com>
2025-08-06 00:06:23 +0900

    refactor: combine DTAGenericPage and content

--- components/dta/pages/DTAGenericPage.tsx
+++ components/dta/pages/DTAGenericPage.tsx
@@ -1,7 +1,10 @@
+import Image from "next/image";
 import { notFound } from "next/navigation";
+import { Fragment } from "react";
 import { getDTAPage } from "@/api/getDTAPage";
 import ViewTransitionTipinPage from "@/components/ViewTransitionTipinPage";
-import { DTAGenericPageContent } from "@/components/dta/pages/DTAGenericPageContent";
+import { DTACarousel } from "@/components/dta/components/DTACarousel";
+import { cn } from "@/lib/cn";
 import { routes } from "@/lib/routes";
 
 export async function DTAGenericPage({ slug }: { slug: string }) {
@@ -17,7 +20,45 @@ export async function DTAGenericPage({ slug }: { slug: string }) {
       title={page.pageTitle ?? ""}
       ancestors={[{ title: "DTA Archive", path: routes.ground("dta") }]}
     >
-      <DTAGenericPageContent page={page} />
+      <div className={cn("flex flex-col gap-20 p-24 700:gap-30")}>
+        <h1>{page.pageTitle}</h1>
+        {page.dynamicZone.map((component) => (
+          <Fragment key={component.id}>
+            {component.__component === "dynamic-zone.subhead-module" && (
+              <h2>{component.subhead}</h2>
+            )}
+            {component.__component === "dynamic-zone.text-module" && (
+              <div>{/* {component.text} */}</div>
+            )}
+            {component.__component === "dynamic-zone.image-carousel-module" && (
+              <DTACarousel
+                className={cn("-mx-24")}
+                carouselSlides={(component.carouselImages ?? []).map((img) => ({
+                  imgSrc: img.url,
+                  imgAlt: img.alternativeText ?? "",
+                }))}
+              />
+            )}
+            {component.__component === "dynamic-zone.text-image-module" && (
+              <div className={cn("grid grid-cols-1 1280:grid-cols-2")}>
+                <div className={cn("w-full body")}>{/* {component.} */}</div>
+                <div className="flex justify-center pt-20 1280:pt-0">
+                  {component.image && (
+                    <Image
+                      src={component.image.url}
+                      alt={component.image.alternativeText ?? ""}
+                      width={component.image.width}
+                      height={component.image.height}
+                      sizes="100vw, (min-width: 1280px) 50vw"
+                      className={cn("")}
+                    />
+                  )}
+                </div>
+              </div>
+            )}
+          </Fragment>
+        ))}
+      </div>
     </ViewTransitionTipinPage>
   );
 }
