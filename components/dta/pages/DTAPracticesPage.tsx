48f8e2014637c695ea08b3125b1300caf7502383
jess vanam <32110513+jvanam@users.noreply.github.com>
2025-08-04 22:34:48 -0400

    add back accidentally-deleted practices page, debug build error

---
+++ components/dta/pages/DTAPracticesPage.tsx
@@ -0,0 +1,64 @@
+import * as React from "react";
+import { z } from "zod/v4";
+import { getDTAPractices } from "@/api/getDTAPractices";
+import { getDTASnippets } from "@/api/getDTASnippet";
+import ViewTransitionTipinPage from "@/components/ViewTransitionTipinPage";
+import {
+  DTAPracticesTable,
+  DTAPracticesTableRow,
+} from "@/components/dta/components/DTAPracticesTable";
+import { DTAContentSingleCol } from "@/components/dta/layouts/DTAContentSingleCol";
+import { cn } from "@/lib/cn";
+import { routes } from "@/lib/routes";
+import { SearchParams } from "@/lib/types";
+import { parseUrlSearchParams } from "@/lib/urlUtils";
+
+export async function DTAPracticesPage({
+  searchParams,
+}: {
+  searchParams: SearchParams;
+}) {
+  const urlSearchParams = parseUrlSearchParams(await searchParams);
+  const page =
+    z.coerce.number().int().min(1).safeParse(urlSearchParams.get("1_page"))
+      .data ?? 1;
+
+  const [
+    {
+      data: practices,
+      meta: { pagination },
+    },
+    { data: snippets },
+  ] = await Promise.all([
+    getDTAPractices({
+      pagination: { page, pageSize: 30 },
+    }),
+    getDTASnippets(),
+  ]);
+
+  return (
+    <ViewTransitionTipinPage
+      type="1"
+      bg="var(--color-dta-tipin-1-background)"
+      fg="var(--color-dta-practices-foreground)"
+      title="Practices"
+      ancestors={[{ title: "DTA Archive", href: routes.ground("dta") }]}
+    >
+      <div className={cn("700:gap-30 flex flex-col gap-20 p-24")}>
+        <h1>Practices</h1>
+        <DTAContentSingleCol>
+          {snippets?.practicesLandingDescription ?? ``}
+        </DTAContentSingleCol>
+        <span className="mono mt-[10px]">All {pagination.total} records</span>
+        <DTAPracticesTable>
+          {practices.map((practice) => (
+            <DTAPracticesTableRow
+              key={practice.documentId}
+              practice={practice}
+            />
+          ))}
+        </DTAPracticesTable>
+      </div>
+    </ViewTransitionTipinPage>
+  );
+}
--- components/globals/components/MediaGalleryNew.tsx
+++ components/globals/components/MediaGalleryNew.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Image from "next/image";
 import React, { useState } from "react";
 import { cn } from "@/lib/cn";
