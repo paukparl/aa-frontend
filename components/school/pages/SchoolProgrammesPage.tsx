eb4fa1898553bd763617cbee4de9d310b735c8a0
Park <paukparliament@gmail.com>
2025-08-06 02:48:08 +0900

    feat: add preliminary school schemas and page templates

---
+++ components/school/pages/SchoolProgrammesPage.tsx
@@ -0,0 +1,11 @@
+import ViewTransitionGroundPage from "@/components/ViewTransitionGroundPage";
+
+export default async function SchoolProgrammesPage() {
+  // TODO: (Paul) Fetch and pass data below
+
+  return (
+    <ViewTransitionGroundPage panel="school">
+      {/* TODO: (Jess) Page goes here */}
+    </ViewTransitionGroundPage>
+  );
+}
