733a07baf22f960aa19c71b53f72458dcdb4288e
Park <paukparliament@gmail.com>
2025-08-09 04:16:06 +0900

    fix: make PREVIEW_SECRET client only

--- lib/serverEnv.ts
+++ lib/serverEnv.ts
@@ -2,13 +2,15 @@ import { z } from "zod/v4";
 
 export default z
   .object({
-    PORT: z.number(),
+    PORT: z.string(),
     BACKEND_API_URL: z.url(),
     BACKEND_API_TOKEN: z.string(),
+    PREVIEW_SECRET: z.string(),
   })
   .parse({
     // Redefine variables here b/c Next.js only inlines variables that are used.
     PORT: process.env.PORT,
     BACKEND_API_URL: process.env.BACKEND_API_URL,
     BACKEND_API_TOKEN: process.env.BACKEND_API_TOKEN,
+    PREVIEW_SECRET: process.env.PREVIEW_SECRET,
   });
