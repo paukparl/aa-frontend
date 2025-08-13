fe9b488c21797f681ca35dad9dde1704116cd75e
jess vanam <32110513+jvanam@users.noreply.github.com>
2025-08-13 12:17:05 -0400

    remove unused utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,20 +1,5 @@
 import { Schema } from "@/lib/schemas";
 
-export const truncate = (text: string, length: number) => {
-  return text.length > length ? text.slice(0, length) + "..." : text;
-};
-
-export const formatNumber = (
-  number: number,
-  locale: string = "en-US",
-): string => {
-  return new Intl.NumberFormat(locale, {
-    style: "decimal",
-    minimumFractionDigits: 0,
-    maximumFractionDigits: 2,
-  }).format(number);
-};
-
 export const formatLocationText = (
   online: boolean | null | undefined,
   locations: Schema<"dtaLocation">[] | undefined,
