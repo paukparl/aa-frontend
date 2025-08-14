4863af67e73c0c6f7684a040e378a2c60b19e7b6
jess vanam <32110513+jvanam@users.noreply.github.com>
2025-08-13 20:58:15 -0400

    merge

--- components/school/components/ProgrammeTableVisitingSchool.tsx
+++ components/school/components/ProgrammeTableVisitingSchool.tsx
@@ -6,11 +6,7 @@ import { Schema } from "@/lib/schemas";
 
 export type CourseUnit = {
   representativeImage: string | undefined;
-<<<<<<< Updated upstream
-  imgAlt: string | null;
-=======
   imgAlt: string | null | undefined;
->>>>>>> Stashed changes
   title: string | null;
   dateTextOverride: string | null;
   location: string;
@@ -50,7 +46,6 @@ export const ProgrammeTableVisitingSchool = ({
             Theme
           </div>
         </div>
-<<<<<<< Updated upstream
         {units.map(
           (unit) =>
             unit.slug && (
@@ -66,9 +61,9 @@ export const ProgrammeTableVisitingSchool = ({
                 <div className="block flex items-start border-y border-r border-dotted p-[15px] body">
                   {unit.representativeImage && (
                     <Image
-                  width={67}
-                  height={67}
-                  alt={unit.imgAlt ?? ``}
+                      width={67}
+                      height={67}
+                      alt={unit.imgAlt ?? ``}
                       src={unit.representativeImage}
                       className="mr-[20px] hidden sm:block"
                     />
@@ -87,43 +82,6 @@ export const ProgrammeTableVisitingSchool = ({
               </Link>
             ),
         )}
-=======
-        {units.map(
-          (unit) =>
-            unit.slug && (
-              <Link
-                href={routes.tipin2(
-                  "school-programmes",
-                  "aa-visiting-school",
-                  unit.slug,
-                )}
-                key={unit.title}
-                className="mb-[-1px] block grid cursor-pointer grid-cols-[1fr_1fr] transition-[.1s] hover:bg-school-visiting-school-bg sm:min-h-[86px] md:grid-cols-[1fr_1fr_1fr] lg:grid-cols-[1fr_1fr_1fr_1fr]"
-              >
-                <div className="block flex items-start border-y border-r border-dotted p-[15px] body">
-                  {unit.representativeImage && (
-                    <Image
-                      alt={unit.imgAlt ?? ``}
-                      fill
-                      src={unit.representativeImage}
-                      className="mr-[20px] hidden h-[67px] w-[67px] object-cover object-center sm:block"
-                    />
-                  )}
-                  {unit.title}
-                </div>
-                <div className="hidden border-y border-dotted p-[15px] body md:block md:border-r">
-                  {unit.dateTextOverride}
-                </div>
-                <div className="block border-y border-dotted p-[15px] body md:border-r">
-                  {unit.location}
-                </div>
-                <div className="hidden border-y border-dotted p-[15px] body lg:block">
-                  {unit.theme}
-                </div>
-              </Link>
-            ),
-        )}
->>>>>>> Stashed changes
       </div>
     </div>
   );
